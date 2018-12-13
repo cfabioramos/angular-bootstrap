76873592499387db3dfe6fdcae5af666f3a1e65e
Carlos Fabio <cfabioramos@gmail.com>
2018-12-13 16:06:01 -0200

    Conclusao do upload das imagens

--- social-network/server.js
+++ social-network/server.js
@@ -2,18 +2,34 @@ const path = require('path');
 const fs = require('fs');
 const express = require('express');
 const multer = require('multer');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+
 const app = express();
 const router = express.Router();
+const BASE_IMAGES_DIR = 'src/assets/img';
+let atualTimeDirectory = null;
+let atualImagesDir = null;
 
-const DIR = 'src/assets/img';
+function defineAtualFileDir(fileDir) {
+	atualTimeDirectory = fileDir;
+	atualImagesDir = BASE_IMAGES_DIR + '/' + atualTimeDirectory;
+	
+	if (!fs.existsSync(atualImagesDir)){
+	    fs.mkdirSync(atualImagesDir);
+	}
+}
+
 let imageName = null;
 let storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, DIR);
+    	let dateNow = new Date();
+    	let fileDir = dateNow.getMonth() + '-' + dateNow.getFullYear();
+    	defineAtualFileDir(fileDir);
+    	
+    	cb(null, atualImagesDir);
     },
     filename: (req, file, cb) => {
-    	imageName = file.fieldname + '-' + Date.now() + '.' + path.extname(file.originalname);
+    	imageName = file.fieldname + '-' + Date.now() + path.extname(file.originalname);
     	cb(null, imageName);
     }
 });
@@ -21,6 +37,7 @@ let upload = multer({storage: storage});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(BASE_IMAGES_DIR));
  
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
@@ -29,23 +46,24 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
+
+//TODO Por enquanto não iremos utilizar
+//app.get('/api', function (req, res) {
+//  res.end('file catcher example');
+//};
  
-app.get('/api', function (req, res) {
-  res.end('file catcher example');
-});
- 
-app.post('/api/upload',upload.single('photo'), function (req, res) {
+app.post('/api/upload', upload.single('photo'), function (req, res) {
     if (!req.file) {
-        console.log("No file received");
+//        console.log("No file received");
         return res.send({
           success: false
         });
     
       } else {
-        console.log('file received');
+//        console.log('file received');
         return res.send({
           success: true,
-          imageName: imageName
+          imageName: atualTimeDirectory + '/' + imageName
         })
       }
 });
--- social-network/src/app/message-poc/message-poc.component.ts
+++ social-network/src/app/message-poc/message-poc.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {  FileUploader, FileSelectDirective } from 'ng2-file-upload/ng2-file-upload';
 
-const folderImagePath = 'assets/img/';
+const urlFileSystem = 'http://localhost:3000/';
 
 @Component({
   selector: 'app-message-poc',
@@ -10,40 +10,40 @@ const folderImagePath = 'assets/img/';
 })
 export class MessagePocComponent implements OnInit {
     
-    private mainImgUrl: string = folderImagePath + 'photo-1544621059634..jpg';
-    private uploadImageUrl: string = 'http://localhost:3000/api/upload';
+    private mainImgUrl: string = urlFileSystem;
+    private uploadImageUrl: string = urlFileSystem + 'api/upload';
 
     public uploader: FileUploader = 
         new FileUploader({url: this.uploadImageUrl, itemAlias: 'photo'});
 
     constructor() { 
-        
+        this.mainImgUrl += 'base/' + this.getRandomInt(1, 5) + '.jpg';
     }
 
     ngOnInit() {
         this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
         this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
-             console.log('ImageUpload:uploaded:', item);
-             console.log('Status: ', status);
-             console.log('Response: ', response);
+//             console.log('ImageUpload:uploaded:', item);
+//             console.log('Status: ', status);
+//             console.log('Response: ', response);
              let jsonResponse = JSON.parse(response);
-             this.mainImgUrl = folderImagePath + jsonResponse.imageName;
+             this.mainImgUrl = urlFileSystem + jsonResponse.imageName;
          };
     }
     
+    getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
     uploadImage() {
-        
-        console.log(this.uploader);
-        
-        this.uploadImageUrl += '?imageName=testeImg.jpg';
-        
         // Update uploader URL
         this.uploader.setOptions({ url: this.uploadImageUrl });
         
         this.uploader.uploadAll();
-        
         // Clear the item queue
-        //this.uploader.clearQueue();
+        // this.uploader.clearQueue();
     }
 
 }
