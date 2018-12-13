9fbaa0cb772efd00cac86ee434454765cc22e748
Carlos Fabio <cfabioramos@gmail.com>
2018-12-13 11:47:16 -0200

    Inicio da insercao de imagem

--- social-network/src/app/app.module.ts
+++ social-network/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MessagePocComponent } from './message-poc/message-poc.component';
 import { MessageFormComponent } from './message-form/message-form.component';
+import { FileSelectDirective } from 'ng2-file-upload';
 
 const appRoutes: Routes = [{ path: 'message', component: MessagePocComponent },
 { path: 'new-message', component: MessageFormComponent }];
@@ -13,6 +15,7 @@ const appRoutes: Routes = [{ path: 'message', component: MessagePocComponent },
 @NgModule( {
     declarations: [
         AppComponent,
+        FileSelectDirective,
         HeaderComponent,
         FooterComponent,
         MessagePocComponent,
@@ -20,6 +23,7 @@ const appRoutes: Routes = [{ path: 'message', component: MessagePocComponent },
     ],
     imports: [
         BrowserModule,
+        FormsModule,
         RouterModule.forRoot(
             appRoutes,
             { enableTracing: true } // <-- debugging purposes only
--- social-network/src/app/message-poc/message-poc.component.ts
+++ social-network/src/app/message-poc/message-poc.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {  FileUploader, FileSelectDirective } from 'ng2-file-upload/ng2-file-upload';
+
+const folderImagePath = 'assets/img/';
 
 @Component({
   selector: 'app-message-poc',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-poc.component.css']
 })
 export class MessagePocComponent implements OnInit {
+    
+    private mainImgUrl: string = folderImagePath + 'photo-1544621059634..jpg';
+    private uploadImageUrl: string = 'http://localhost:3000/api/upload';
 
-  constructor() { }
+    public uploader: FileUploader = 
+        new FileUploader({url: this.uploadImageUrl, itemAlias: 'photo'});
 
-  ngOnInit() {
-  }
+    constructor() { 
+        
+    }
+
+    ngOnInit() {
+        this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+        this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+             console.log('ImageUpload:uploaded:', item);
+             console.log('Status: ', status);
+             console.log('Response: ', response);
+             let jsonResponse = JSON.parse(response);
+             this.mainImgUrl = folderImagePath + jsonResponse.imageName;
+         };
+    }
+    
+    uploadImage() {
+        
+        console.log(this.uploader);
+        
+        this.uploadImageUrl += '?imageName=testeImg.jpg';
+        
+        // Update uploader URL
+        this.uploader.setOptions({ url: this.uploadImageUrl });
+        
+        this.uploader.uploadAll();
+        
+        // Clear the item queue
+        //this.uploader.clearQueue();
+    }
 
 }
