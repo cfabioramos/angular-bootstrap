285407ccd181634b88e7074be87d7f30c6c9f35a
Carlos Fabio <cfabioramos@gmail.com>
2018-12-18 17:13:12 -0200

    Novos testes drag and drop com o interactjs

--- angular-base-project/src/app/test-draggable/test-draggable.component.ts
+++ angular-base-project/src/app/test-draggable/test-draggable.component.ts
@@ -2,49 +2,52 @@ import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 import * as interact from 'interactjs';
 
-@Component({
-  selector: 'app-test-draggable',
-  templateUrl: './test-draggable.component.html',
-  styleUrls: ['./test-draggable.component.css']
-})
+@Component( {
+    selector: 'app-test-draggable',
+    templateUrl: './test-draggable.component.html',
+    styleUrls: ['./test-draggable.component.css']
+} )
 export class TestDraggableComponent implements OnInit {
 
-  constructor() { }
+    // mainContainer = $('#mainImage');
 
-  ngOnInit() {
-      interact( '.draggable' )
-      .draggable( {
-          // enable inertial throwing
-          inertia: true,
-          // keep the element within the area of it's parent
-          restrict: {
-              restriction: "parent",
-              endOnly: true,
-              elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-          },
-          // enable autoScroll
-          autoScroll: true,
+    constructor() {
+    }
 
-          // call this function on every dragmove event
-          onmove: this.dragMoveListener
-      } );
-  }
-  
-  dragMoveListener( event ) {
-      var target = event.target,
-          // keep the dragged position in the data-x/data-y attributes
-          x = ( parseFloat( target.getAttribute( 'data-x' ) ) || 0 ) + event.dx,
-          y = ( parseFloat( target.getAttribute( 'data-y' ) ) || 0 ) + event.dy;
+    ngOnInit() {
+        interact( '.draggable' )
+            .draggable({
+                // enable inertial throwing
+                // inertia: true,
+                // keep the element within the area of it's parent
+//                          restrict: {
+//                              restriction: "parent",
+//                              endOnly: true,
+//                              elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+//                          },
+                // enable autoScroll
+                autoScroll: true,
 
-      // translate the element
-      target.style.webkitTransform =
-          target.style.transform =
-          'translate(' + x + 'px, ' + y + 'px)';
+                // call this function on every dragmove event
+                onmove: this.dragMoveListener
+            });
+    }
 
-      // update the position attributes
-      target.setAttribute( 'data-x', x );
-      target.setAttribute( 'data-y', y );
-  }
-  
+    dragMoveListener( event ) {
+        var target = event.target,
+            // keep the dragged position in the data-x/data-y attributes
+            x = ( parseFloat( target.getAttribute( 'data-x' ) ) || 0 ) + event.dx,
+            y = ( parseFloat( target.getAttribute( 'data-y' ) ) || 0 ) + event.dy;
+
+        // translate the element
+        target.style.webkitTransform =
+            target.style.transform =
+            'translate(' + x + 'px, ' + y + 'px)';
+
+        // update the position attributes
+        target.setAttribute( 'data-x', x );
+        target.setAttribute( 'data-y', y );
+    }
+
 
 }
